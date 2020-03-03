ce923986737cc7d5b8bac0a20d92615142fbd754
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-03-04 04:47:57 +0600

    create user is done

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
 
 export class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+
+
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+
+    onSubmit = (e) => {
+        e.preventDefault(); // prevent html default behavior
+        const user = {
+            username: this.state.username
+        };
+        console.log(user);
+        this.setState({
+            username: ''
+        });
+    }
+
+
     render() {
         return (
             <div>
                 <h2>Create User</h2>
+                <br /><br />
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Input
+                        fluid
+                        required
+                        label="User"
+                        placeholder="Enter a user"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}
+                    />
+                    <Button inverted fluid color='teal' type="submit" >Submit</Button>
+                </Form>
             </div>
         )
     }
